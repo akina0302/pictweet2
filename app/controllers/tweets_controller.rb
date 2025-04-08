29a59a1e4b108b309c3e0388d1618db90f49456a
akina0302 <ymu.m24@gmail.com>
2025-04-08 22:52:04 +0900

    tweetの保存

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,23 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+    redirect_to '/'
+
+
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:name,:image,:texit)
+  end
+
+
 end
